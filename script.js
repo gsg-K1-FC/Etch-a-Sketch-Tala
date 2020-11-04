4276e2c50e19b2135f5497942b832b4e1eb1f9a8
talaalsweiti <talaalsweiti@gmail.com>
2020-11-04 20:27:49 +0200

    Edits on the grids #3

--- script.js
+++ script.js
@@ -2,19 +2,18 @@ const container = document.getElementById("container");
 let size = 16;
 
 
-
-
 function creatGrid(size) {
-    container.style.setProperty('--grid-rows', size);
-    container.style.setProperty('--grid-cols', size);
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++){
+  container.style.setProperty('--grid-rows', size);
+  container.style.setProperty('--grid-cols', size);
+  container.style.gridTemplateRows = `repeat(${size},1fr)`;
+  container.style.gridTemplateColumns = `repeat(${size},1fr)`;
+
+  for (let i = 0; i < Math.pow(size,2) ; i++) {
+
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
-    }
+
   };
 };
-
 creatGrid(size);
-
 
